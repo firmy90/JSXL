a3aa459fe59945b30c69ceb30b5d33d9e7e8e746
firmy90 <firmy90@protonmail.com>
2023-04-18 13:36:09 +0200

    fetch

--- weather-app/app4-fetch-yargs.js
+++ weather-app/app4-fetch-yargs.js
@@ -5,5 +5,6 @@ fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${command}&appid=932e9946953aa6fbcc5b20db4e8051a0`
   )
     .then((response) => response.json())
-    .then((data) => {console.log(data); console.log(data.coord.lat)})
+    // .then((data) => {console.log(data); console.log(data.coord.lat)})
+    .then((d) => {console.log(d.weather[0].description)})
     .catch((error) => console.log(error));
