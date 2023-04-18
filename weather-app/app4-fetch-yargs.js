68ad73caad5169317fb0e9f2168f4e2072a0dc1c
firmy90 <firmy90@protonmail.com>
2023-04-18 13:40:28 +0200

    fetch

--- weather-app/app4-fetch-yargs.js
+++ weather-app/app4-fetch-yargs.js
@@ -2,7 +2,7 @@ const yargs = require('yargs');
 
 let command = process.argv[2];
 fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${command}&appid=932e9946953aa6fbcc5b20db4e8051a0`
+    `https://api.openweathermap.org/data/2.5/weather?q=${command}&appid={api_key}`
   )
     .then((response) => response.json())
     // .then((data) => {console.log(data); console.log(data.coord.lat)})
