816312b7756f040ec27666756b777e4da24e29ee
firmy90 <firmy90@protonmail.com>
2023-04-19 10:30:09 +0200

    nodejs app use

--- node-web-server/routing.js
+++ node-web-server/routing.js
@@ -1,16 +1,5 @@
 const express = require("express");
-const hbs = require("hbs");
 let app = express();
-let p = 3000;
-
-app.listen(p, () => {
-  console.log(`Serwer dziala na porcie ${p}`);
-});
-app.use(express.static(__dirname + "/public"));
-app.set("view engine", "hbs");
-hbs.registerPartials(__dirname + "/views/partials");
-hbs.registerHelper(); 
-
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
@@ -26,7 +15,11 @@ app.get("/bad", (req, res) => {
 app.get("/person", (req, res) => {
   res.render("person.hbs", {
     pageTitle: "PersonPage",
-    currentYear: new Date().getFullYear(),
     message: "Hello there!",
   });
 });
+
+module.export={
+    app,
+
+}
--- node-web-server/server2.js
+++ node-web-server/server2.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const hbs = require("hbs");
+const routing = require("./routing");
+const fs = require("fs");
 let app = express();
 let p = 3000;
 
@@ -7,9 +9,32 @@ app.listen(p, () => {
   console.log(`Serwer dziala na porcie ${p}`);
 });
 app.use(express.static(__dirname + "/public"));
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFileSync("server.log", log + "\n", (err) => {
+    if (err) {
+      console.log("Unable to append server.log");
+    }
+  });
+  next();
+});
+
 app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + "/views/partials");
-hbs.registerHelper(); 
+
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", (text) => {
+  return text.toUpperCase();
+});
+
+
+
+
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
@@ -26,7 +51,6 @@ app.get("/bad", (req, res) => {
 app.get("/person", (req, res) => {
   res.render("person.hbs", {
     pageTitle: "PersonPage",
-    currentYear: new Date().getFullYear(),
     message: "Hello there!",
   });
 });
